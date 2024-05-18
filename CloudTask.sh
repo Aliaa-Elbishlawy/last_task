16da44189d52573a50d5826bf3184c2349fb275d
Aliaa Mohammed <110140230+Aliaa-Elbishlawy@users.noreply.github.com>
2024-05-18 11:21:17 +0300

    Update CloudTask.sh

--- CloudTask.sh
+++ CloudTask.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-# This script lists the files in the current directory 
 
-ls
+# Clone the second repository
+git clone https://github.com/Aliaa-Elbishlawy/Parking-System.git another_repo
+
+# Navigate to the second repository's directory
+cd another_repo
+
+# Ensure the script in the second repository is executable
+chmod +x AnotherScript.sh
+
+# Execute the script in the second repository
+./AnotherScript.sh
